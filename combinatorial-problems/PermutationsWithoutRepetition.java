ce1c2a42ee4f3c36136f78ba5ce9113e633b376d
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-04-22 11:01:20 +0300

    Permutations without Repetition

---
+++ combinatorial-problems/PermutationsWithoutRepetition.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PermutationsWithoutRepetition {
+    public static String[] elements;
+    public static String[] permutations;
+    public static boolean[] used;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        elements = scanner.nextLine().split("\\s+");
+
+        permutations = new String[elements.length];
+        used = new boolean[elements.length];
+        permute(0);
+    }
+
+    private static void permute(int index) {
+        if (index == elements.length) {
+            print();
+            return;
+        }
+
+        for (int i = 0; i < elements.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                permutations[index] = elements[i];
+                permute(index + 1);
+                used[i] = false;
+            }
+        }
+    }
+
+    private static void print() {
+        System.out.println(String.join(" ", permutations));
+    }
+}

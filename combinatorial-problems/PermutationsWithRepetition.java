1f485056b64b414df9b5876a18295570c9b1cda8
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-04-22 11:49:06 +0300

    Permutations with Repetition

--- combinatorial-problems/PermutationsWithRepetition.java
+++ combinatorial-problems/PermutationsWithRepetition.java
@@ -1,37 +1,45 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class PermutationsWithRepetition {
     public static String[] elements;
-    public static String[] permutations;
-    public static boolean[] used;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         elements = scanner.nextLine().split("\\s+");
 
-        permutations = new String[elements.length];
-        used = new boolean[elements.length];
         permute(0);
     }
 
     private static void permute(int index) {
         if (index == elements.length) {
-            print();
+            print(elements);
             return;
         }
 
-        for (int i = 0; i < elements.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
-                permutations[index] = elements[i];
+        permute(index + 1);
+        Set<String> swapped = new HashSet<>();
+        swapped.add(elements[index]);
+
+        for (int i = index + 1; i < elements.length; i++) {
+            if (!swapped.contains(elements[i])) {
+                swap(elements, index, i);
                 permute(index + 1);
-                used[i] = false;
+                swap(elements, index, i);
+                swapped.add(elements[i]);
             }
         }
     }
 
-    private static void print() {
-        System.out.println(String.join(" ", permutations));
+    private static void swap(String[] arr, int first, int second) {
+        String temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static void print(String[] arr) {
+        System.out.println(String.join(" ", arr));
     }
 }

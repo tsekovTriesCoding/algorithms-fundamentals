2a42e98752e1b66133afbd1976d89f1f1bc6ec13
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-04-23 16:19:13 +0300

    Combinations with Repetitions

---
+++ combinatorial-problems/CombinationsWithRepetition.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CombinationsWithRepetition {
+    public static String[] elements;
+    public static String[] combinations;
+    public static boolean[] used;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        elements = scanner.nextLine().split("\\s+");
+        int k = Integer.parseInt(scanner.nextLine());
+
+        combinations = new String[k];
+        used = new boolean[elements.length];
+
+        combinations(0, 0);
+    }
+
+    private static void combinations(int index, int start) {
+        if (index == combinations.length) {
+            print(combinations);
+        } else {
+            for (int i = start; i < elements.length; i++) {
+                combinations[index] = elements[i];
+                combinations(index + 1, i);
+            }
+        }
+    }
+
+    private static void print(String[] variations) {
+        System.out.println(String.join(" ", variations));
+    }
+}

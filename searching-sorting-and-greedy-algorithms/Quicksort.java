9973130e5c752564f3546b16ef0362c1a20de808
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-05-06 20:23:24 +0300

    Quicksort

---
+++ searching-sorting-and-greedy-algorithms/Quicksort.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Quicksort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        sort(array);
+
+        System.out.println(Arrays.toString(array).replaceAll("[\\[\\],]", ""));
+    }
+
+    private static void sort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+
+}
